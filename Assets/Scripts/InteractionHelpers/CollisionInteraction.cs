893b4e512f09c9a7c1b1cd3d6a8566720656cc6d
zrei <88964793+zrei@users.noreply.github.com>
2025-03-26 15:06:33 +0800

    fix double collision bug

--- Assets/Scripts/InteractionHelpers/CollisionInteraction.cs
+++ Assets/Scripts/InteractionHelpers/CollisionInteraction.cs
@@ -1,5 +1,5 @@
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 /// <summary>
 /// Simple collision interaction - triggering the collider will instantly call step completion
@@ -10,6 +10,9 @@ public class CollisionInteraction : MonoBehaviour
 {
     [SerializeField] private LayerMask m_AllowedLayers;
     [SerializeField] private bool m_DestroyCollidedObject = false;
+    [SerializeField] private bool m_LimitByTag = false;
+    [Tooltip("Ignored if LimitByTag is not true")]
+    [SerializeField] private string[] m_AllowedTags;
 
     /// <summary>
     /// For additional hook-ons
@@ -18,16 +21,22 @@ public class CollisionInteraction : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //Logger.Log(typeof(CollisionInteraction), this.gameObject, "Trigger enter", LogLevel.LOG);
-        if (((1 << other.gameObject.layer) & m_AllowedLayers) != 0)
+        if ((((1 << other.gameObject.layer) & m_AllowedLayers) != 0) && (!m_LimitByTag || m_AllowedTags.Contains(other.gameObject.tag)))
         {
             if (m_DestroyCollidedObject)
             {
                 Destroy(other.gameObject);
             }
-            //Logger.Log(typeof(CollisionInteraction), this.gameObject, other.gameObject.name + ", position: " + other.gameObject.transform.position, LogLevel.LOG);
             OnCollisionInteraction?.Invoke();
             GlobalEvents.StepsEvents.OnCompleteStep?.Invoke();
         }
     }
+
+#if UNITY_EDITOR
+    public void OnValidate()
+    {
+        if (GetComponentInChildren<Collider>() == null)
+            Logger.Log(typeof(CollisionInteraction), this.gameObject, "Cannot find a collider!", LogLevel.ERROR);
+    }
+#endif
 }
