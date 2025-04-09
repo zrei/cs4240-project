9b3eec9e1d60f04118bfac55fe2e1d969f0e461d
zrei <88964793+zrei@users.noreply.github.com>
2025-04-09 22:46:46 +0800

    change dirty skin to time based

--- Assets/Scripts/Parts/DirtySkin.cs
+++ Assets/Scripts/Parts/DirtySkin.cs
@@ -4,21 +4,24 @@ public class DirtySkin : MonoBehaviour
 {
     [SerializeField] private CircularFill m_CircularFill;
     [SerializeField] private GameObject m_DirtyObj;
+    [SerializeField] private float m_TimeToClean = 5f;
 
     [SerializeField] private SoundCue m_CleanSkinSound;
 
     private bool m_IsCompleted = false;
+    private bool m_IsInContact = false;
+    private float m_TimeInContact = 0f;
 
     private void Start()
     {
-        m_CircularFill.OnFillPercentage += OnComplete;
+        // m_CircularFill.OnFillPercentage += OnComplete;
         m_CircularFill.OnContactStartEvent += OnContactStart;
         m_CircularFill.OnContactEndEvent += OnContactEnd;
     }
 
     private void OnDestroy()
     {
-        m_CircularFill.OnFillPercentage -= OnComplete;
+        // m_CircularFill.OnFillPercentage -= OnComplete;
         m_CircularFill.OnContactStartEvent -= OnContactStart;
         m_CircularFill.OnContactEndEvent -= OnContactEnd;
     }
@@ -38,10 +41,26 @@ public class DirtySkin : MonoBehaviour
         {
             m_CleanSkinSound.ToggleSoundPlaying(true);
         }
+
+        m_IsInContact = true;
     }
 
     private void OnContactEnd()
     {
         m_CleanSkinSound.StopSound();
+        m_IsInContact = false;
+    }
+
+    private void Update()
+    {
+        if (!m_IsCompleted && m_IsInContact)
+        {
+            m_TimeInContact += Time.deltaTime;
+
+            if (m_TimeInContact > m_TimeToClean)
+            {
+                OnComplete();
+            }
+        }
     }
 }
