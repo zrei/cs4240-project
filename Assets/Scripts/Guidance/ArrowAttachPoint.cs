4fbf06ec33b175bbe926b06cb01dd397cc54106e
zrei <88964793+zrei@users.noreply.github.com>
2025-04-09 22:33:21 +0800

    arrow changes

---
+++ Assets/Scripts/Guidance/ArrowAttachPoint.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+/// <summary>
+/// This is just used to indicate which game object to attach to
+/// </summary>
+public class ArrowAttachPoint : MonoBehaviour { }
---
+++ Assets/Scripts/Guidance/ArrowBillboardScript.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+/// <summary>
+/// Script to help rotate the arrow - the arrow head will continue to point towards the target, but it will also rotate towards the camera
+/// </summary>
+public class ArrowBillboardScript : MonoBehaviour
+{
+    [SerializeField] private Transform m_TargetA;
+    [SerializeField] private Transform m_TargetB;
+    [SerializeField] private float m_ArrowAnimateCycle = 5f;
+    [SerializeField] private float m_ArrowAnimateAmplitude = 10f;
+
+    private Camera m_MainCamera;
+    private Transform m_ChildSpriteRenderer;
+    private float m_CurrentAnimateTime = 0f;
+    private float m_ArrowAnimateDirection = 1f;
+
+    private void Start()
+    {
+        m_MainCamera = Camera.main;
+        m_ChildSpriteRenderer = transform.GetChild(0);
+        SetTarget(m_TargetA);
+    }
+
+    private void OnDisable()
+    {
+        m_CurrentAnimateTime = 0f;
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.FromToRotation(-Vector3.up, m_TargetA.position - transform.position);
+        transform.localRotation = Quaternion.Euler(transform.localEulerAngles.x, 0, transform.localEulerAngles.z);
+
+        m_CurrentAnimateTime += Time.deltaTime;
+        if (m_CurrentAnimateTime > m_ArrowAnimateCycle)
+        {
+            m_CurrentAnimateTime -= m_ArrowAnimateCycle;
+            m_ArrowAnimateDirection *= -1;
+        }
+        float height = m_CurrentAnimateTime * m_ArrowAnimateAmplitude * m_ArrowAnimateDirection;
+        m_ChildSpriteRenderer.localPosition = new Vector3(0f, height, 0f);
+    }
+
+    private void SetTarget(bool targetA)
+    {
+        Transform targetTransform = targetA ? m_TargetA : m_TargetB;
+        Transform newParent = targetTransform.GetComponentInChildren<ArrowAttachPoint>(true).transform;
+        transform.parent = newParent;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+    }
+
+    public void OnHoverEnter()
+    {
+        SetTarget(m_TargetB);
+    }
+
+    public void OnHoverExit()
+    {
+        SetTarget(m_TargetA);
+    }
+}
