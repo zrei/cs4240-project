8decd52f3bf97939555e56c9a028e58dcc0b5400
zrei <88964793+zrei@users.noreply.github.com>
2025-04-14 18:30:32 +0800

    fix some main menu errors and add fade to black

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -1,11 +1,17 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup _blackOutCg;
+    [SerializeField] private float m_BlackOutTime = 1f;
+
+    private bool m_IsLoading = false;
+
     public void PlayGame() 
     {
-        SceneManager.LoadSceneAsync("EntryScene");
+        LoadScene("EntryScene");
     }
    public void QuitGame()
    {
@@ -13,12 +19,34 @@ public class MainMenu : MonoBehaviour
    }
    public void BathroomStage()
    {
-    SceneManager.LoadSceneAsync("Bathroom_set(interior)");
+    LoadScene("Bathroom_set(interior)");
    }
    public void RestaurantStage()
    {
-    SceneManager.LoadSceneAsync("RestrauntScene");
+    LoadScene("RestrauntScene");
    }
+
+    private void LoadScene(string sceneName)
+    {
+        if (m_IsLoading)
+            return;
+
+        m_IsLoading = true;
+        StartCoroutine(LoadSceneCoroutine(sceneName));
+    }
+
+    private IEnumerator LoadSceneCoroutine(string sceneName)
+    {
+        float t = 0f;
+       _blackOutCg.blocksRaycasts = true;
+        while (t < m_BlackOutTime)
+        {
+            yield return null;
+            t += Time.deltaTime;
+            _blackOutCg.alpha = Mathf.Lerp(0f, 1f, t / m_BlackOutTime);
+        }
+        SceneManager.LoadSceneAsync(sceneName);
+    }
    // if aimed at, change appearance
     // if not, change back
    // if interacted with, activate
--- Assets/Scripts/Main Menu/ReturnToMenuButton.cs
+++ Assets/Scripts/Main Menu/ReturnToMenuButton.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,6 +6,9 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class ReturnToMenuButton : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup m_Cg;
+    [SerializeField] private float m_BlackOutTime = 1f;
+
     private Button m_Button;
 
     private void Start()
@@ -22,6 +26,19 @@ public class ReturnToMenuButton : MonoBehaviour
     private void ReturnToMainMenu()
     {
         m_Button.onClick.RemoveAllListeners();
+        StartCoroutine(BlackOutCoroutine());
+    }
+
+    private IEnumerator BlackOutCoroutine()
+    {
+        float t = 0f;
+        m_Cg.blocksRaycasts = true;
+        while (t < m_BlackOutTime)
+        {
+            yield return null;
+            t += Time.deltaTime;
+            m_Cg.alpha = Mathf.Lerp(0f, 1f, t / m_BlackOutTime);
+        }
         SceneManager.LoadScene(0);
     }
 }
