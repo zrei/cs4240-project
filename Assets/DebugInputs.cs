901a85ef5f802082d70a80ceb714c11026102f28
zrei <88964793+zrei@users.noreply.github.com>
2025-04-15 15:17:04 +0800

    update steps

--- Assets/DebugInputs.cs
+++ Assets/DebugInputs.cs
@@ -7,6 +7,7 @@ public class DebugInputs : MonoBehaviour
     [SerializeField] private bool _enable = false;
     [SerializeField] private InputActionReference _quitAction;
     [SerializeField] private InputActionReference _resetAction;
+    [SerializeField] private InputActionReference _backToMenuAction;
 
      // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
@@ -18,12 +19,18 @@ public class DebugInputs : MonoBehaviour
         }
         _quitAction.action.performed += OnQuit;
         _resetAction.action.performed += OnReset;
+
+        if (_backToMenuAction)
+            _backToMenuAction.action.performed += BackToMenu;
     }
 
     private void OnDestroy()
     {
         _quitAction.action.performed -= OnQuit;
         _resetAction.action.performed -= OnReset;
+
+        if (_backToMenuAction)
+            _backToMenuAction.action.performed -= BackToMenu;
     }
 
     private void OnQuit(InputAction.CallbackContext callbackContext)
@@ -34,5 +41,10 @@ public class DebugInputs : MonoBehaviour
     private void OnReset(InputAction.CallbackContext callbackContext)
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void BackToMenu(InputAction.CallbackContext callbackContext)
+    {
+        SceneManager.LoadScene(0);
     }
 }
